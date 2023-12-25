2ec61f0229f3684deab2d7235d63d434b1a908a5
KirillPuteev0 <154738859+KirillPuteev0@users.noreply.github.com>
2023-12-25 16:31:40 +0300

    Add files via upload

---
+++ main-project/SourceFiles/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #1. Library Subscription\n";
+    cout << "Author: Valentinovitch Daniil\n";
+    cout << "Group: 23Ping1d\n";
+    return 0;
+}
